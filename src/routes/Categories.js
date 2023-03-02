764a1e6204edb7ab955ab6ba495063e06943ab9c
mrprotocoll <lekanvgbg@gmail.com>
2023-03-02 18:49:24 +0100

    Fix code review correction to display under construction

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,12 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from 'redux/categories/categoriesSlice';
 
 const Categories = () => {
   const dispatch = useDispatch();
-
+  const { categories } = useSelector((store) => store.category);
   return (
     <div className="container">
       <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
+      <span>{categories}</span>
     </div>
   );
 };
