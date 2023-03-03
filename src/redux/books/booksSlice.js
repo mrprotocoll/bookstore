4962ff95335175d9b5b260667f80bdd49cee5d1d
mrprotocoll <lekanvgbg@gmail.com>
2023-03-03 11:30:28 +0100

    Add remove book functionality from server

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const APIKEY = 'DZhozfmIef5jvfNuBg7r';
-const URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APIKEY}/books`;
+const URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APIKEY}/books/`;
 
 export const getBooks = createAsyncThunk('book/getBooks', async () => {
   try {
@@ -14,17 +14,27 @@ export const getBooks = createAsyncThunk('book/getBooks', async () => {
 
 export const addBook = createAsyncThunk('book/addBook', async (data, thumkAPI) => {
   try {
-    const addNewBook = await fetch(URL, {
+    return fetch(URL, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
-    });
-    if (addNewBook.status === 201) {
-      return data;
-    }
-    return {};
+    }).then(() => data);
+  } catch (error) {
+    return thumkAPI.rejectWithValue('Oops! something went wrong');
+  }
+});
+
+export const removeBook = createAsyncThunk('book/removeBook', async (id, thumkAPI) => {
+  try {
+    return fetch(URL + id, {
+      method: 'DELETE',
+      body: JSON.stringify({ item_id: id }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then(() => id);
   } catch (error) {
     return thumkAPI.rejectWithValue('Oops! something went wrong');
   }
@@ -38,11 +48,6 @@ const initialState = {
 const booksSlice = createSlice({
   name: 'book',
   initialState,
-  reducers: {
-    removeBook: (state, { payload }) => {
-      state.books = state.books.filter((book) => book.item_id !== payload);
-    },
-  },
   extraReducers: {
     [getBooks.fulfilled]: (state, action) => {
       state.books = action.payload;
@@ -60,13 +65,10 @@ const booksSlice = createSlice({
         category,
       }];
     },
-    [addBook.rejected]: (state) => {
-      state.error = true;
-      console.log('rejected');
+    [removeBook.fulfilled]: (state, { payload }) => {
+      delete state.books[payload];
     },
   },
 });
-
-export const { removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
